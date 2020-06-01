e8b138e1c9995cfeef98ceee255676474419f9fc
Bharath <bharathk@thoughtworks.com>
2020-06-02 00:50:05 +0800

    [1][Bharathan] - add userId field to testresults table

--- database/migrations/20200530030540-create-test-result.js
+++ database/migrations/20200530030540-create-test-result.js
@@ -20,6 +20,10 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
